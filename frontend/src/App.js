4615df49e0a601dc71e5be68f39589451e4c4748
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2022-02-10 10:37:05 +0100

    #B1 Update layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Login from './components/Login.js';
 
 export default function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
 
 export default function Login() {
-    return (any);
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validate() {
+        return (
+            phoneNumber.length === 8 
+            && 
+            (password.length > 0 && password.length <= 10)
+        );
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    
+    return (
+        <div className="Login">
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" controlId="phoneNumber">
+            <Form.Label className="phonelabel">Phone Number</Form.Label>
+            <Form.Control
+                autoFocus
+                type="phoneNumber"
+                placeholder="Phone Number"
+                value={phoneNumber}
+                onChange={(n) => setPhoneNumber(n.target.value)}
+            />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+            <Form.Label className="passwordlabel">Password</Form.Label>
+            <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(p) => setPassword(p.target.value)}
+            />
+            </Form.Group>
+            <Button block size="lg" type="submit" disabled={!validate()} className="Button">
+            Login
+            </Button>
+        </Form>
+        </div>
+    );
 }
