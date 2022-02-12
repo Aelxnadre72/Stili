6b4112fa5b8ef7960ee16671e3f4c4437d09f3ad
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2022-02-12 11:17:38 +0100

    #B1 Format code

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Axios from 'axios';
-import './Login.css';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Axios from "axios";
+import "./Login.css";
 
 export default function Login() {
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [text, setText] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
 
-    function validate () {
-        return (
-            phoneNumber === "12345678" 
-            && password === "hund123"
-        )
-    }
+  function validate() {
+    Axios.get("user.json").then((res) => {
+      if (data.length === 0) {
+        setData(res.data.data[0]);
+      }
+    });
+    return phoneNumber === data.phoneNumber && password === data.password;
+  }
 
-    const changeText = (textinput) => setText(textinput);
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        /*Axios.post('URL', {
+  function handleSubmit(event) {
+    event.preventDefault();
+    /*Axios.post('URL', {
             'phoneNumber' : phoneNumber,
             'password' : password,
         },
@@ -32,40 +33,50 @@ export default function Login() {
         )
         .then(res => console.log(res))
         .catch(error => console.err(error))*/
-    }
+  }
 
-    return (
-        <div className="Login">
-        <Form onSubmit={handleSubmit}>
-            <Form.Group size="lg" controlId="phoneNumber">
-            <Form.Label className="phonelabel">Phone Number</Form.Label>
-            <Form.Control
-                autoFocus
-                type="phoneNumber"
-                placeholder="Phone Number"
-                value={phoneNumber}
-                onChange={(n) => setPhoneNumber(n.target.value)}
-            />
-            </Form.Group>
-            <Form.Group size="lg" controlId="password">
-            <Form.Label className="passwordlabel">Password</Form.Label>
-            <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(p) => setPassword(p.target.value)}
-            />
-            </Form.Group>
-            <p className="errormsg">{text}</p>
-            <Button block size="lg" type="submit" className="Button" onClick={
+  const changeText = (textinput) => setText(textinput);
 
-                validate() 
-                ? () => changeText("Suksess")
-                : () => changeText("Incorrect phone number/password, please try again.")}>
-
-            Login
-            </Button>
-        </Form>
-        </div>
-    );
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="phoneNumber">
+          <Form.Label className="phonelabel">Phone Number</Form.Label>
+          <Form.Control
+            autoFocus
+            type="phoneNumber"
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChange={(n) => setPhoneNumber(n.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="passwordlabel">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(p) => setPassword(p.target.value)}
+          />
+        </Form.Group>
+        <p className="errormsg">{text}</p>
+        <Button
+          block
+          size="lg"
+          type="submit"
+          className="Button"
+          onClick={
+            validate()
+              ? () => changeText("Success! Redirecting...")
+              : () =>
+                  changeText(
+                    "Incorrect phone number/password, please try again."
+                  )
+          }
+        >
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
