c4e0ea6e6e525843cd6b446fa0b75849c6a29075
elintov <elintov@stud.ntnu.no>
2022-02-14 12:41:00 +0100

    #2 enkel brukerklasse

--- DjangoAPI/stili/views.py
+++ DjangoAPI/stili/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def __str__(self):
+        return self.username
+
+class Admin(User):
+    pass
+
