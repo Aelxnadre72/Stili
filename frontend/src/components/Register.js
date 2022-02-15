be4a4a3197dbf014c67e4424b8f2f6133b4dd417
elintov <elintov@stud.ntnu.no>
2022-02-15 13:31:28 +0100

    #2 Register og Login side enkelt design

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -36,64 +36,61 @@ export default function Login() {
   const changeText = (textinput) => setText(textinput);
 
   return (
+    <div class="box-form">
+    <><div class="left">
+      <div class="overlay">
+        <h1>Stili.</h1>
+        <p class="joinFun">Registrer deg nå og bli med på morroa!</p>
+      </div>
+    </div>
     <div className="Register">
+    <h5>Register</h5>
+    <p>Already have an account? <a href="Login.js">Click here</a> to log in!</p>
       <Form onSubmit={handleSubmit}>
         <Form.Group size="lg" controlId="name">
-          <Form.Label className="name">Name</Form.Label>
           <Form.Control
             autoFocus
             type="name"
             placeholder="Name"
             value={name}
-            onChange={(n) => setName(n.target.value)}
-          />
+            onChange={(n) => setName(n.target.value)} />
         </Form.Group>
         <Form.Group size="lg" controlId="phoneNumber">
-          <Form.Label className="phoneNumber">Phone Number</Form.Label>
           <Form.Control
             autoFocus
             type="phoneNumber"
             placeholder="Phone Number"
             value={phoneNumber}
-            onChange={(p) => setPhoneNumber(p.target.value)}
-          />
+            onChange={(p) => setPhoneNumber(p.target.value)} />
           <Form.Group size="lg" controlId="age">
-            <Form.Label className="age">Age</Form.Label>
             <Form.Control
               autoFocus
               type="age"
               placeholder="Age"
               value={age}
-              onChange={(a) => setAge(a.target.value)}
-            />
+              onChange={(a) => setAge(a.target.value)} />
           </Form.Group>
         </Form.Group>
         <Form.Group size="lg" controlId="experience">
-          <Form.Label className="experience">Experience</Form.Label>
           <Form.Control
             type="experience"
             placeholder="Experience"
             value={experience}
-            onChange={(e) => setExperience(e.target.value)}
-          />
+            onChange={(e) => setExperience(e.target.value)} />
         </Form.Group>
         <Form.Group size="lg" controlId="location">
-          <Form.Label className="location">Location</Form.Label>
           <Form.Control
             type="location"
             placeholder="Location"
             value={location}
-            onChange={(l) => setLocation(l.target.value)}
-          />
+            onChange={(l) => setLocation(l.target.value)} />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
-          <Form.Label className="password">Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
             value={password}
-            onChange={(p) => setPassword(p.target.value)}
-          />
+            onChange={(p) => setPassword(p.target.value)} />
         </Form.Group>
         <p className="errormsg">{text}</p>
         <Button
@@ -101,15 +98,14 @@ export default function Login() {
           size="lg"
           type="submit"
           className="Button"
-          onClick={
-            validate()
-              ? () => changeText("Success! Redirecting...")
-              : () => changeText("Make sure all the fields are filled in.")
-          }
+          onClick={validate()
+            ? () => changeText("Success! Redirecting...")
+            : () => changeText("Make sure all the fields are filled in.")}
         >
           Register
         </Button>
       </Form>
+    </div></>
     </div>
   );
 }
