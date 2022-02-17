4a4fca606a619a5dae9835c631fd2293b8455a52
alexle <alexle@stud.ntnu.no>
2022-02-17 15:46:42 +0100

    #11

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -50,7 +50,7 @@ export default function Login() {
       <div className="Login">
         <h5>Login</h5>
         <p>Don't have an account? <Link to="/register">Click here</Link> to sign up!</p>
-          <Form onSubmit={handleSubmit}>
+          <Form>
             <Form.Group size="lg" controlId="phoneNumber">
               <Form.Control
                 autoFocus
