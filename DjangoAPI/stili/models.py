f17be919c759ce8fd4ec3912aa350301295a5357
Odin Strand Gladsø <odinsgl@stud.ntnu.no>
2022-02-17 11:07:34 +0100

    #12 Changed model field values
    Removed default value and added blank=True instead.

--- DjangoAPI/stili/models.py
+++ DjangoAPI/stili/models.py
@@ -11,13 +11,13 @@ class User(models.Model):
         VETERAN = '3', _('Veteran')
 
 
-    firstName = models.CharField(max_length=100, default='firstname')
-    surname = models.CharField(max_length=100, default='surname')
-    phoneNumber = models.TextField(default='00000000')
-    age = models.IntegerField(default=18)
-    experience = models.IntegerField(choices=Experience.choices, default=1)
-    location = models.TextField(default='location')
-    password = models.TextField(default='password')
+    firstName = models.CharField(max_length=100, blank = False)
+    surname = models.CharField(max_length=100, blank = False)
+    phoneNumber = models.TextField(primary_key=True)
+    age = models.IntegerField(blank = False)
+    experience = models.IntegerField(choices=Experience.choices, blank = False)
+    location = models.TextField(blank = False)
+    password = models.TextField(blank = False)
 
     def __str__(self):
         return self.firstName
