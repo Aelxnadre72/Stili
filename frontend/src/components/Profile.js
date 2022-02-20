df4f97728ae5c97a73d687dcf7f56bc004da28ba
AndBreis <andbreis@stud.ntnu.no>
2022-02-20 21:02:39 +0100

    Profilside med info hentet fra et array i JSON-format

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,28 +1,80 @@
 import React, { useState } from "react";
+import Image from 'react-bootstrap/Image';
 import picture from "../../src/PicPlaceholder.png";
+import Form from "react-bootstrap/Form";
 import "./Profile.css";
 
 
-export default function Profile(){
-    const [profilePic, setProfilePic] = useState("");
-    const [username, setUsername] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [age, setAge] = useState("");
-    const [hikingExperience, setHikingExperience] = useState("");
-    const [location, setLocation] = useState("");
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
+export default function Profile(){
+    const user = {
+        userName: "Brukernavn",
+        firstName: "Navn",
+        surname: "Navnesen",
+        phoneNumber: "12345678",
+        age: "50",
+        experience: "Some",
+        location: "Trondheim",
+    };
+    
+  
 
     return(
-        <div className="Profile">
-            <img className="ProfilePic" id="ProfilePic" src={picture}></img>
-            <p className="username">username</p>
-            <p className="phoneNumber">phoneNumber</p>
-            <p className="age">age</p>
-            <p className="hikingExperience">hikingExperience</p>
-            <p className="location">location</p>
+        <div className="Box-form">
+            <>
+                <div className="overlay">
+                    <h1>Stili.Profili</h1>
+                </div>
+                <Image id="Profilbilde" src={picture}>
+                </Image>
+                <div className = "Profile">
+                    <h5>{user.userName}</h5>
+                    <Form>
+                        <Form.Group size="lg" controlId="firstName">
+                            <Form.Control
+                            autoFocus
+                            value={"Firstname:" + " " + user.firstName}
+                            disabled
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="surName">
+                            <Form.Control
+                            autoFocus
+                            value={"Surname:" + " " + user.surname}
+                            disabled
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="phoneNumber">
+                            <Form.Control
+                            autoFocus
+                            value={"Phonenumber:" + " " + user.phoneNumber}
+                            disabled
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="age">
+                            <Form.Control
+                            autoFocus
+                            value={"Age:" + " " + user.age}
+                            disabled
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="experience">
+                            <Form.Control
+                            autoFocus
+                            value={"Experience:" + " " + user.experience}
+                            disabled
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="location">
+                            <Form.Control
+                            autoFocus
+                            value={"Location:" + " " + user.location}
+                            disabled
+                            />
+                        </Form.Group>
+                    </Form>
+                </div>
+            </>
         </div>
     );
 }
