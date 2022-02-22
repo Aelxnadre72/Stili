2509b444b9485186ba682904d27fbe0c9f483beb
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2022-02-22 12:31:12 +0100

    Update project

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Card(props) {
+  return (
+    <div className={props.name}>
+      <li className="card">
+        <Link className="card_link" to={props.path}>
+          <figure className={props.wrapper} data-category={props.label}>
+            <img src={props.src} alt="test" className="card_image"></img>
+          </figure>
+          <div className="card_info">
+            <h5 className="card_text">{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </div>
+  );
+}
