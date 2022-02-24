fd121c8393c0f5434ee12165c5fe9dcce9043ca8
Selma <selma.gudmundsen@gmail.com>
2022-02-24 12:47:09 +0100

    POST and GET requests added

--- DjangoAPI/stili/views.py
+++ DjangoAPI/stili/views.py
@@ -39,3 +39,18 @@ def events(request):
     context = {}
     return render(request, "index.html", context)
 
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def event(request):
+    if request.method == 'GET':
+        user = Event.objects.all()
+        serializer = EventSerializer(user, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
