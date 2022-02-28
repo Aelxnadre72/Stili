637d14ec7de2e9bec183eae85d07e4a5db4da11e
Odin Strand Gladsø <odinsgl@stud.ntnu.no>
2022-02-28 18:36:43 +0100

    #8 Added navbars
    Added navbar and topnavbar to profile page

--- frontend/src/components/ProfileInfo.js
+++ frontend/src/components/ProfileInfo.js
@@ -1,34 +1,42 @@
 import React from "react";
 import Image from 'react-bootstrap/Image';
 import picture from "../../src/PicPlaceholder.png";
+import Navbar from "./Navbar";
+import TopNavbar from "./TopNavbar";
 
 export default function ProfileInfo(props) {
 
      if(props.user != null){
           if(props.canEdit) { //render fields and buttons so user can edit instead of plain text if canEdit is true.
                return(
-                    <>
-                    <div className="ProfilePage">
-                         <Image className="ProfilePic" src={picture}>
-                         </Image>
-                         <h3>Textbox example <b>with editing rights</b></h3>
-                         <div className="example-text-box">
-                         <p>{props.user.firstName}</p>
-                         <p>{props.user.surname}</p>
-                         <p>{props.user.age}</p>
-                         <p>{props.user.experience}</p>
-                         <p>{props.user.location}</p>
-                         <p>{props.user.password}</p>
-                         <p>{props.user.phoneNumber}</p>
-                         <p>{props.user.isAdmin}</p>
+                    <div>
+                         <>
+                         <TopNavbar />
+                         <Navbar />
+                         <div className="ProfilePage">
+                              <Image className="ProfilePic" src={picture}>
+                              </Image>
+                              <h3>Textbox example <b>with editing rights</b></h3>
+                              <div className="example-text-box">
+                              <p>{props.user.firstName}</p>
+                              <p>{props.user.surname}</p>
+                              <p>{props.user.age}</p>
+                              <p>{props.user.experience}</p>
+                              <p>{props.user.location}</p>
+                              <p>{props.user.password}</p>
+                              <p>{props.user.phoneNumber}</p>
+                              <p>{props.user.isAdmin}</p>
+                              </div>
                          </div>
+                         </>
                     </div>
-                    </>
                )
           }
           else {
                return(
                     <>
+                    <TopNavbar />
+                    <Navbar />
                     <div className="ProfilePage">
                          <Image className="ProfilePic" src={picture}>
                          </Image>
