0757981c9ed80673a3b9e90b9ff5fa0ecfccbc4b
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2022-03-13 15:06:30 +0100

    #B2 Add popup

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -5,11 +5,59 @@ import campus from "./Campusrunden.jpg";
 import Axios from "axios";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function Cards() {
   const [data, setData] = useState([]);
   const [q, setQ] = useState("");
+  const [open, setOpen] = useState(false);
+  const [experience, setExperience] = useState("Easy");
+  const [location, setLocation] = useState("Trondheim");
+
+  const locations = [
+    {
+      value: "Trondheim",
+      label: "Trondheim",
+    },
+    {
+      value: "Oslo",
+      label: "Oslo",
+    },
+    {
+      value: "Bergen",
+      label: "Bergen",
+    },
+    {
+      value: "Stavanger",
+      label: "Stavanger",
+    },
+  ];
+
+  const experiences = [
+    {
+      value: "1",
+      label: "Easy",
+    },
+    {
+      value: "2",
+      label: "Mediocre",
+    },
+    {
+      value: "3",
+      label: "Veteran",
+    },
+  ];
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     Axios({
@@ -34,20 +82,108 @@ export default function Cards() {
 
   return (
     <div className="all">
-      <div className="search-box">
-      </div>
+      <div className="search-box"></div>
       <div className="cards">
         <div className="card_container">
-        <div className="search">
+          <div className="search">
             <TextField
-          id="outlined-basic"
-          variant="outlined"
-          size="small"
-          label="Search"
-          onChange={(e) => setQ(e.target.value)}
-          />
-          <Link to="/registerEvent"><Button id="button" variant="contained" size="small" color="success">Create Event</Button></Link>
-        </div>
+              id="outlined-basic"
+              variant="outlined"
+              size="small"
+              label="Search"
+              onChange={(e) => setQ(e.target.value)}
+            />
+            <Button
+              id="button"
+              variant="contained"
+              size="small"
+              color="success"
+              onClick={handleClickOpen}
+            >
+              Create Event
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Event</DialogTitle>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="normal"
+                  id="name"
+                  label="Name"
+                  type="name"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="normal"
+                  id="date"
+                  type="date"
+                  variant="standard"
+                />
+                <TextField
+                  id="outlined-select-location-native"
+                  select
+                  size="small"
+                  label="Location"
+                  value={location}
+                  onChange={(l) => setLocation(l.target.value)}
+                  SelectProps={{
+                    native: true,
+                  }}
+                >
+                  {locations.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+                <TextField
+                  id="outlined-select-experience-native"
+                  select
+                  size="small"
+                  label="Experience"
+                  value={experience}
+                  onChange={(e) => setExperience(e.target.value)}
+                  SelectProps={{
+                    native: true,
+                  }}
+                >
+                  {experiences.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+                <TextField
+                  margin="normal"
+                  id="distance"
+                  label="Distance"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="normal"
+                  id="filled-multiline-flexible"
+                  label="Description"
+                  style={{ width: 552 }}
+                  multiline
+                  maxRows={4}
+                  variant="outlined"
+                />
+                <TextField
+                  margin="normal"
+                  id="size"
+                  label="Size"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Submit</Button>
+              </DialogActions>
+            </Dialog>
+          </div>
           <div className="card_wrapper">
             <ul className="card_items">
               {data
