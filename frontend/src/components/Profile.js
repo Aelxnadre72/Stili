1a001739e79aed917bec822b8b18ce0969cbfdf3
Selma <selma.gudmundsen@gmail.com>
2022-03-13 15:45:21 +0100

    #32 Made commercial user information visible on profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -5,6 +5,7 @@ import ProfileInfo from "./ProfileInfo";
 
 export default function Profile(){
   const [user, setUser] = useState(null);
+  const [commercialUser, setCommercialUser] = useState(null);
   let navigate = useNavigate();
   let loc = useLocation()
   const [canEdit, setCanEdit] = useState(false);
@@ -13,24 +14,35 @@ export default function Profile(){
         if (localStorage.getItem("id") == null || loc.state === null) { // can not see profiles if not logged in or from link
             navigate('/Home');
             }
-            const profileID = loc.state; // the <link> from the previous page has to send the phonenumber to this page, sends 1 if it is the "my profile" link in Navbar.js
-            var profileNumber = "";
-            if (profileID === "1") {
-                profileNumber = localStorage.getItem("id");
-                setCanEdit(true);
+            if (localStorage.getItem("id").length == 9) {
+              const profileNumber = localStorage.getItem("id");
+              fetchCommericalData(profileNumber);
+
+            } else {
+              const profileID = loc.state; // the <link> from the previous page has to send the phonenumber to this page, sends 1 if it is the "my profile" link in Navbar.js
+              var profileNumber = "";
+              if (profileID === "1") {
+                  profileNumber = localStorage.getItem("id");
+                  setCanEdit(true);
+              }
+              else {
+                  profileNumber = profileID;
+              }
+              fetchUserData(profileNumber);
             }
-            else {
-                profileNumber = profileID;
-            }
-            fetchUserData(profileNumber);
     }, []);
 
     const  fetchUserData = (profileNumber) => {
         getUser().then(response => {
-            console.log(response.find(o => o.phoneNumber === profileNumber))
             setUser(response.find(o => o.phoneNumber === profileNumber));
         });
     }
+
+    const  fetchCommericalData = (profileNumber) => {
+      getCommercialUser().then(response => {
+        setCommercialUser(response.find(o => o.orgNumber === profileNumber));
+      });
+  }
 
     async function getUser() {
         try {
@@ -48,7 +60,23 @@ export default function Profile(){
         }
       }
 
+      async function getCommercialUser() {
+        try {
+        const response = await Axios({
+          method: "GET",
+          url:"/commercialUsers/",
+          responseType:"json"
+          })
+        return response.data;
+        }
+        catch(error){
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      }
+
     return(
-        <ProfileInfo canEdit = {canEdit} user = {user}/>
+        <ProfileInfo canEdit = {canEdit} user = {user} commercialUser = {commercialUser}/>
     );
 }
