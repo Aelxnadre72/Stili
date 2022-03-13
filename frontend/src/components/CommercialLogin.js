8242c007bf39d5351788a040ffa6f12cf27075b7
elintov <elintov@stud.ntnu.no>
2022-03-13 13:13:28 +0100

    Fixed commercial login

--- frontend/src/components/CommercialLogin.js
+++ frontend/src/components/CommercialLogin.js
@@ -70,10 +70,10 @@ export default function Login() {
       </div>
       <div className="Login">
         <h5>Login</h5>
-        <p><Link to="/register">Click here</Link> to sign up!</p>
-        <p><Link to="">Click here</Link> to log in</p>
-        <p>Are you a commecial organization? <Link to="/commercialRegister">Click here</Link> to register</p>
-        <p>Don't have an account? <Link to="/home">Click here</Link></p>
+        <div className="information">
+          <p>Don't have an account? <Link to="/register">Click here</Link> to sign up!</p>
+          <p>Not a commecial organization? Click to <Link to="/login">log in</Link> or to <Link to="/login">register</Link></p>
+        </div>
         <Form>
             <Form.Group size="lg" controlId="orgNumber">
               <Form.Control
